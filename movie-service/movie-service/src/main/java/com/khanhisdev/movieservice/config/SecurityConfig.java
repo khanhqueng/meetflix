f063a908963524eca78c0eb8708af56a57f367d6
khanhqueng <quangkhanh288123@gmail.com>
2024-09-23 01:26:41 +0700

    add custom access deny

--- movie-service/movie-service/src/main/java/com/khanhisdev/movieservice/config/SecurityConfig.java
+++ movie-service/movie-service/src/main/java/com/khanhisdev/movieservice/config/SecurityConfig.java
@@ -1,11 +1,8 @@
 package com.khanhisdev.movieservice.config;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.khanhisdev.movieservice.exception.AuthException.AccessDeniedExceptionHandler;
-import com.khanhisdev.movieservice.exception.AuthException.AuthenticationExceptionHandler;
+import com.khanhisdev.movieservice.exception.AuthException.CustomAccessDeniedHandler;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -13,18 +10,11 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -65,6 +55,7 @@ public class SecurityConfig {
 
                         .anyRequest()
                         .authenticated())
+                .exceptionHandling(exception-> exception.accessDeniedHandler(new CustomAccessDeniedHandler()))
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(Customizer.withDefaults()));
         return http.build();
